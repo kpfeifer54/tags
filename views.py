9aaeb94c8e461d5e52732384ab064c7ca6f06569
Julie Stone <JulieEStone@gmail.com>
2021-03-23 10:28:09 -0600

    Merge branch 'master' of https://github.com/kpfeifer54/tags

--- views.py
+++ views.py
@@ -5,8 +5,16 @@ from tag import Tag
 
 @app.route('/', methods=['GET'])
 def show_tags():
+<<<<<<< HEAD
     g.setdefault('image', app.config['config']['awesome_image'])
     return render_template('index.html', tags=Tag.select())
+=======
+    tags = Tag.select()
+    tags_html = '\n'.join(list(map(lambda x: x.name + "<br>", tags)))
+    form_html = "<form action=\"/tags\" method=\"POST\"><label>Enter a tag: </label><input name=\"tag-name\"></form>"
+    #embed()
+    return "<html><head><link href=\"static/style.css\" rel=\"stylesheet\" type=\"text/css\"></head><body><h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div></body></html>" % (app.config['config']['awesome_image'],form_html, tags_html)
+>>>>>>> 5da88e4d62ea57b203b72010f573c747988d8cb9
 
 
 @app.route('/tags', methods=['POST'])
